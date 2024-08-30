d45f654337bdb4f3d82640be97dc7c3569632f70
Matej Seifert <matej.seifert@hamiltonjet.nz>
2024-08-30 19:03:55 +1200

    added coords loading

--- src/main/java/com/caliberly/WinCombination.java
+++ src/main/java/com/caliberly/WinCombination.java
@@ -5,6 +5,9 @@ import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.Setter;
 
+import java.awt.geom.Point2D;
+import java.util.List;
+
 @AllArgsConstructor
 @Getter
 public class WinCombination {
@@ -12,9 +15,9 @@ public class WinCombination {
 	@Setter
 	private String name;
 	//	private double reward_multiplier;
-//	private int count;
+	private int count = 0;
 	@Setter
 	private CombinationWhen when;
-//	private String group;
-//	private String covered_areas;
+	private String group; //not necessary, but loaded
+	private List<List<String>> covered_areas;
 }
